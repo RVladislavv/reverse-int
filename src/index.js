bbd0c27ae9d3ce2a0cbc9b8de233004d54346704
Vladislav Rychkov <vvrych3@gmail.com>
2022-06-22 20:02:23 +0500

    first version

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let res = '';
+    n = n * (-1);
+    while (n % 10 == 0) {
+        n = n / 10;
+    }
+    n = n + '';
+    n = n.split().reverse().join("");
+    return n;
 }
